5bbf3d6c50e8552c7e6375769b150c1cb0ab66c0
Max Gopey <max.gopey@symmetrics.de>
2017-02-09 17:08:51 +0200

    Add scroll to day row when clicking day in callendar

--- public/javascripts/front.js
+++ public/javascripts/front.js
@@ -1,16 +1,55 @@
 (function() {
     "use strict";
 
+    let scrolledToDay;
     let actions = {
-        showShowDetails: function() {
-
+        toggleShowDetails: function($showRow) {
+            $showRow.toggleClass('detailed');
+        },
+        scrollToDay: function(day) {
+            scrolledToDay = day;
+            let $dayNode = $('.date-' + day);
+            if ($dayNode.length) {
+                $("html, body").animate({
+                    scrollTop: $dayNode.offset().top - $('.main-header').height()
+                }, '300', 'swing');
+            }
+        },
+        fixScrolledToDay: function() {
+            if (scrolledToDay) {
+                if (!isSmallViewport()) {
+                    setTimeout(() => actions.scrollToDay(scrolledToDay), 300);
+                }
+            }
         }
     };
 
+    function isSmallViewport() {
+        return false; //ResponsiveBootstrapToolkit.is('xs') || ResponsiveBootstrapToolkit.is('sm');
+    }
+
+    function fixPosterMargin() {
+        if (isSmallViewport()) {
+            let tries = 5;
+            let interval = setInterval(function() {
+                let headerHeight = $('.main-header').height();
+                $('.scroll-top').css({'top': headerHeight});
+                $('.filters-col').css({'margin-top': headerHeight});
+                if (!--tries) {
+                    clearInterval(interval);
+                }
+            }, 500);
+        }
+    }
+
+
     $('.show-title a').click(function(event) {
         event.preventDefault();
-        let $showRow = ($(event.target).closest('.show-row'));
-        $showRow.toggleClass('detailed');
-    })
+        actions.toggleShowDetails($(event.target).closest('.show-row'));
+    });
+
+    $('.calendar .day').click(function(event) {
+        actions.scrollToDay($(event.target).data('day'));
+    });
 
 })(jQuery);
