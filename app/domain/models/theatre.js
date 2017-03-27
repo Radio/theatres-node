7956621d56cb7b4a8ddce1ccd063c89a499ee6d0
Max Gopey <max.gopey@symmetrics.de>
2017-03-27 13:18:46 +0300

    Add karabas hall id to theatre model and admin

--- app/domain/models/theatre.js
+++ app/domain/models/theatre.js
@@ -8,7 +8,8 @@ let theatreSchema = new Schema({
     title: { type: String, required: true },
     url: String,
     hasFetcher: Boolean,
-    houseSlug: String   // The theatre slug in "Actor's House"
+    houseSlug: String,   // The theatre slug in "Actor's House"
+    karabasHallId: String // Hall ID at https://kharkov.karabas.com/theatres/
 });
 theatreSchema.set('toObject', { versionKey: false });
 
@@ -16,8 +17,8 @@ theatreSchema.statics.findByKey = function(key, callback) {
     return this.findOne({ key: key }, callback);
 };
 
-theatreSchema.statics.findByKeyOrHouseSlug = function(key, callback) {
-    return this.findOne({ $or: [{ key: key }, { houseSlug: key }] }, callback);
+theatreSchema.statics.findByAnyKey = function(key, callback) {
+    return this.findOne({ $or: [{ key: key }, { houseSlug: key }, { karabasHallId: key }] }, callback);
 };
 
 theatreSchema.methods.isInHouse = function() {
